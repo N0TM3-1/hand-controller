0c173f7ac4ac8aa13a9037a79d329cb520007b5e
NotMe <112022500+N0TM3-1@users.noreply.github.com>
2025-04-12 10:06:33 +0300

    Added Gestures

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Controller
 
 import disable_warnings
 disable_warnings.disable()
-import pynput
+from pynput.keyboard import Key, Controller
 import cv2
 import mediapipe as mp
 
@@ -11,13 +11,42 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
 # Start capturing video
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 keyboard = Controller()
 def detect_fist(hand_landmarks):
     if hand_landmarks.landmark[8].y > hand_landmarks.landmark[6].y and hand_landmarks.landmark[12].y > hand_landmarks.landmark[10].y and hand_landmarks.landmark[16].y > hand_landmarks.landmark[14].y and hand_landmarks.landmark[20].y > hand_landmarks.landmark[18].y:
         print("Fist detected")
+        keyboard.release('w')
+        keyboard.release('s')
+        keyboard.release('a')
+        keyboard.release('d')
+        keyboard.release(Key.space)
 
-with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7) as hands:
+def detect_index(hand_landmarks):
+    if hand_landmarks.landmark[8].y < hand_landmarks.landmark[6].y and hand_landmarks.landmark[12].y > hand_landmarks.landmark[10].y and hand_landmarks.landmark[16].y > hand_landmarks.landmark[14].y and hand_landmarks.landmark[20].y > hand_landmarks.landmark[18].y:
+        print("Index finger detected")
+        keyboard.press('w')
+
+def detect_two_fingers(hand_landmarks):
+    if hand_landmarks.landmark[8].y < hand_landmarks.landmark[6].y and hand_landmarks.landmark[12].y < hand_landmarks.landmark[10].y and hand_landmarks.landmark[16].y > hand_landmarks.landmark[14].y and hand_landmarks.landmark[20].y > hand_landmarks.landmark[18].y:
+        print("Two fingers detected")
+        keyboard.press('s')
+
+def detect_palm(hand_landmarks):
+    if hand_landmarks.landmark[8].y < hand_landmarks.landmark[6].y and hand_landmarks.landmark[12].y < hand_landmarks.landmark[10].y and hand_landmarks.landmark[16].y < hand_landmarks.landmark[14].y and hand_landmarks.landmark[20].y < hand_landmarks.landmark[18].y:
+        print("Open palm detected")
+        keyboard.press(Key.space)
+
+def detect_pinky(hand_landmarks):
+    if hand_landmarks.landmark[20].y < hand_landmarks.landmark[18].y and hand_landmarks.landmark[8].y > hand_landmarks.landmark[6].y and hand_landmarks.landmark[12].y > hand_landmarks.landmark[10].y and hand_landmarks.landmark[16].y > hand_landmarks.landmark[14].y:
+        if hand_landmarks.landmark[4].x > hand_landmarks.landmark[3].x:
+            print("Thumb pointing right")
+            keyboard.press('d')
+        elif hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x:
+            print("Thumb pointing left")
+            keyboard.press('a')
+
+with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7) as hands:
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -33,7 +62,10 @@ with mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7) as hands:
                 # Draw hand landmarks
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             detect_fist(hand_landmarks)
-
+            detect_index(hand_landmarks)
+            detect_two_fingers(hand_landmarks)
+            detect_palm(hand_landmarks)
+            detect_pinky(hand_landmarks)
 
         cv2.imshow("Hand Tracker", frame)
         if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to exit
